20e81dba4de23e4892fb3dedcd67494576bff93a
pop4959 <pop4959@gmail.com>
2022-08-21 14:38:20 -0700

    Update BlueMap integration

--- src/main/java/org/popcraft/blueborder/BlueBorder.java
+++ src/main/java/org/popcraft/blueborder/BlueBorder.java
@@ -2,23 +2,22 @@ package org.popcraft.blueborder;
 
 import com.flowpowered.math.vector.Vector2d;
 import de.bluecolored.bluemap.api.BlueMapAPI;
-import de.bluecolored.bluemap.api.marker.MarkerAPI;
-import de.bluecolored.bluemap.api.marker.MarkerSet;
-import de.bluecolored.bluemap.api.marker.Shape;
-import de.bluecolored.bluemap.api.marker.ShapeMarker;
+import de.bluecolored.bluemap.api.BlueMapWorld;
+import de.bluecolored.bluemap.api.markers.MarkerSet;
+import de.bluecolored.bluemap.api.markers.ShapeMarker;
+import de.bluecolored.bluemap.api.math.Color;
+import de.bluecolored.bluemap.api.math.Shape;
 import org.bukkit.World;
 import org.bukkit.WorldBorder;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.awt.Color;
-import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
 public final class BlueBorder extends JavaPlugin {
-    private static final String MARKERSET_ID = "worldborder";
+    private static final String MARKER_SET_ID = "worldborder";
     private static final String LABEL = "World border";
 
     @Override
@@ -29,6 +28,7 @@ public final class BlueBorder extends JavaPlugin {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        BlueMapAPI.getInstance().ifPresent(this::removeWorldBorders);
         BlueMapAPI.getInstance().ifPresent(this::addWorldBorders);
         return true;
     }
@@ -39,39 +39,31 @@ public final class BlueBorder extends JavaPlugin {
     }
 
     private void addWorldBorders(BlueMapAPI blueMapAPI) {
-        try {
-            final MarkerAPI markerAPI = blueMapAPI.getMarkerAPI();
-            final MarkerSet markerSet = markerAPI.createMarkerSet(MARKERSET_ID);
-            markerSet.setLabel(LABEL);
-            blueMapAPI.getMaps().forEach(blueMapMap -> {
-                final World world = getServer().getWorld(blueMapMap.getWorld().getUuid());
-                if (world != null) {
-                    final WorldBorder worldBorder = world.getWorldBorder();
-                    final double centerX = worldBorder.getCenter().getX();
-                    final double centerZ = worldBorder.getCenter().getZ();
-                    final double radius = worldBorder.getSize() / 2d;
-                    final Vector2d pos1 = new Vector2d(centerX - radius, centerZ - radius);
-                    final Vector2d pos2 = new Vector2d(centerX + radius, centerZ + radius);
-                    final Shape border = Shape.createRect(pos1, pos2);
-                    final ShapeMarker marker = markerSet.createShapeMarker(world.getName(), blueMapMap, border, world.getSeaLevel());
-                    marker.setColors(Color.RED, new Color(0, true));
-                    marker.setLabel(LABEL);
-                }
-            });
-            markerAPI.save();
-        } catch (IOException e) {
-            getLogger().warning("Failed to add world borders");
+        for (final World world : getServer().getWorlds()) {
+            final MarkerSet markerSet = MarkerSet.builder().label(LABEL).build();
+            final WorldBorder worldBorder = world.getWorldBorder();
+            final double centerX = worldBorder.getCenter().getX();
+            final double centerZ = worldBorder.getCenter().getZ();
+            final double radius = worldBorder.getSize() / 2d;
+            final Vector2d pos1 = new Vector2d(centerX - radius, centerZ - radius);
+            final Vector2d pos2 = new Vector2d(centerX + radius, centerZ + radius);
+            final Shape border = Shape.createRect(pos1, pos2);
+            final ShapeMarker marker = ShapeMarker.builder()
+                    .label(LABEL)
+                    .shape(border, world.getSeaLevel())
+                    .lineColor(new Color(0xFF0000, 1f))
+                    .fillColor(new Color(0))
+                    .lineWidth(3)
+                    .depthTestEnabled(false)
+                    .build();
+            markerSet.getMarkers().put(world.getName(), marker);
+            blueMapAPI.getWorld(world.getName())
+                    .map(BlueMapWorld::getMaps)
+                    .ifPresent(maps -> maps.forEach(map -> map.getMarkerSets().put(MARKER_SET_ID, markerSet)));
         }
     }
 
     private void removeWorldBorders(BlueMapAPI blueMapAPI) {
-        try {
-            final MarkerAPI markerAPI = blueMapAPI.getMarkerAPI();
-            if (markerAPI.removeMarkerSet(MARKERSET_ID)) {
-                markerAPI.save();
-            }
-        } catch (IOException e) {
-            getLogger().warning("Failed to remove world borders");
-        }
+        blueMapAPI.getMaps().forEach(map -> map.getMarkerSets().remove(MARKER_SET_ID));
     }
 }
