ad06721269f6170717d97d4b6b6aa24978b479e1
Dylan Xaldin <Puremin0rez515@gmail.com>
2022-10-01 04:05:02 -0500

    Switch to Kotlin DSL for build scripts

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("java-library")
+}
+
+group = "org.popcraft"
+version = "1.1.0"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+tasks {
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+    }
+    processResources {
+        filesMatching("plugin.yml") {
+            expand("version" to project.version)
+        }
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.19.2-R0.1-SNAPSHOT")
+    compileOnly("com.github.BlueMap-Minecraft:BlueMapAPI:v2.1.0")
+}
